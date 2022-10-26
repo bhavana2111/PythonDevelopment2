66399783e0739eaae71eb19de42a026672f34bb1
bhavana2111 <bhavana2111@gmail.com>
2022-10-26 14:25:07 +0530

    Python File

--- LeetCode/RomanToDecimalConversion.py
+++ LeetCode/RomanToDecimalConversion.py
@@ -2,6 +2,7 @@ class Solution:
     def romanToInt(self, s: str) -> int:
         map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         output = 0
+        // Enter a Roman number.
         s=input("Enter Roman")
 
         s.replace("IV", "IIII")
